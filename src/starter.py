ed7d83aa7e7ee2a660016574ddfe83dd460f822e
animalize <animalize81@gmail.com>
2016-07-04 19:28:33 +0800

    支持zipapp

--- src/starter.py
+++ src/starter.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # coding=utf-8
 
-version = 'InfoPi v.2016-06-17'
+version = 'InfoPi v.2016-07-04'
 
 def main():
     # -------------------
--- src/workers/__init__.py
+++ src/workers/__init__.py
@@ -1,25 +1,10 @@
-from glob import glob
-from keyword import iskeyword
-from os.path import dirname, join, split, splitext
+# coding=utf-8
+import os
+import pkgutil
 
-basedir = dirname(__file__)
+pkgpath = os.path.dirname(__file__)
+pkgname = os.path.basename(pkgpath)
 
-__all__ = []
-for name in glob(join(basedir, '*.py')):
-    module = splitext(split(name)[-1])[0]
-    if not module.startswith('_') and \
-       module.isidentifier() and \
-       not iskeyword(module):
-
-       __import__(__name__+'.'+module)
-       __all__.append(module)
-
-        # try:
-        #     __import__(__name__+'.'+module)
-        # except Exception as e:
-        #     print('无法加载worker: %s' % module)
-        #     print('异常信息:', e, '\n')
-        # else:
-        #     __all__.append(module)
-
-#print('import了%d个worker文件' % len(__all__))
+for _, file, _ in pkgutil.iter_modules([pkgpath]):
+    print(file)
+    __import__(pkgname+'.'+file)

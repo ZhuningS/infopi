023575429563079a2b11164880e3138c5248d3d4
animalize <animalize81@gmail.com>
2016-07-20 20:53:28 +0800

    要求regex的版本不小于2016.07.14

--- src/red.py
+++ src/red.py
@@ -4,7 +4,7 @@ import threading
 try:
     import regex as re
     vt = tuple(int(i.strip()) for i in re.__version__.split('.'))
-    if vt < (2, 4, 85):
+    if vt < (2, 4, 105):
         print('regex版本较低:%s, 使用内置re' % re.__version__)
         raise Exception('regex version is low')
     re.DEFAULT_VERSION = re.VERSION1
